c00bde7b9fd6e025387a1c4220a618b7f6f9dc37
petrubear <edison.martinez@gmail.com>
2018-02-13 18:04:55 -0500

    refactor: add timeout ignore test

--- junitinaction/src/test/java/com/manning/junitbook/ch03/mastering/DefaultControllerTest.java
+++ junitinaction/src/test/java/com/manning/junitbook/ch03/mastering/DefaultControllerTest.java
@@ -1,6 +1,7 @@
 package com.manning.junitbook.ch03.mastering;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -30,13 +31,66 @@ public class DefaultControllerTest {
         Response response = defaultController.processRequest(request);
         assertNotNull("must not be null", response);
         assertEquals("should be sampleresponse class", SampleResponse.class, response.getClass());
+        assertEquals("should be equals", new SampleResponse(), response);
 
+    }
+
+    @Test
+    public void testProcessRequestAnswerErrorResponse() {
+        SampleRequest request = new SampleRequest("testError");
+        SampleExceptionHandler handler = new SampleExceptionHandler();
+        defaultController.addHandler(request, handler);
+        Response response = defaultController.processRequest(request);
+        assertNotNull("must not be null", response);
+        assertEquals(ErrorResponse.class, response.getClass());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetHandlerNotDefined() {
+        SampleRequest request = new SampleRequest("testNotDefined");
+        //this line should throw error
+        defaultController.getHandler(request);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testAddRequestDuplicatedName() {
+        SampleRequest request = new SampleRequest();
+        SampleHandler handler = new SampleHandler();
+        //this line should throw error
+        defaultController.addHandler(request, handler);
+    }
+
+    @Test(timeout = 130)
+    @Ignore(value = "ignore test sample")
+    public void testProcessMultipleRequestTimeout() {
+        Request request;
+        Response response = new SampleResponse();
+        RequestHandler handler = new SampleHandler();
+
+        for (int i = 0; i < 9999; i++) {
+            request = new SampleRequest(String.valueOf(i));
+            defaultController.addHandler(request, handler);
+            response = defaultController.processRequest(request);
+            assertNotNull(response);
+            assertNotSame(ErrorResponse.class, response.getClass());
+        }
     }
 
     //region TestObjects
     private class SampleRequest implements Request {
+        public static final String DEFAULT_NAME = "Test";
+        private String name;
+
+        public SampleRequest(String name) {
+            this.name = name;
+        }
+
+        public SampleRequest() {
+            this.name = DEFAULT_NAME;
+        }
+
         public String getName() {
-            return "Test";
+            return this.name;
         }
     }
 
@@ -48,7 +102,29 @@ public class DefaultControllerTest {
     }
 
     private class SampleResponse implements Response {
+        public static final String NAME = "Test";
 
+        public String getName() {
+            return NAME;
+        }
+
+        public boolean equals(Object object) {
+            boolean result = false;
+            if (object instanceof SampleResponse) {
+                result = ((SampleResponse) object).getName().equals(getName());
+            }
+            return result;
+        }
+
+        public int hashCode() {
+            return NAME.hashCode();
+        }
+    }
+
+    private class SampleExceptionHandler implements RequestHandler {
+        public Response process(Request request) throws Exception {
+            throw new Exception("error processing request");
+        }
     }
     //endregion
 }
