f26587d84adc6e982af6530069600dc4f0d4d780
petrubear <edison.martinez@gmail.com>
2018-02-13 13:16:40 -0500

    refactor: add message

--- junitinaction/src/test/java/emg/demo/unittest/simple/CalculadoraTest.java
+++ junitinaction/src/test/java/emg/demo/unittest/simple/CalculadoraTest.java
@@ -9,6 +9,6 @@ public class CalculadoraTest {
     @Test
     public void testSuma() {
         Calculadora calculadora = new Calculadora();
-        assertEquals(12.0, calculadora.suma(5.0, 7.0), 0);
+        assertEquals("should be the sum", 12.0, calculadora.suma(5.0, 7.0), 0);
     }
 }
